085ad9634f69e0075177224461e8cd9c9c69b5da
pablopr3 <pabloprdev@gmail.com>
2026-03-01 22:16:55 +0000

    variables

---
+++ clase_00_introduccion/introduccion.py
@@ -0,0 +1,18 @@
+print("Hola Mundo")
+
+#esto es un comentario
+# dsaodsao
+#dsadsa
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este también es un
+comentario
+en varias líneas
+'''
+
---
+++ clase_01_variables/variables.py
@@ -0,0 +1,2 @@
+hola = "hola"
+num = 5
