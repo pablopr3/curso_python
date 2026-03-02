0784fa7826587e117a27c4b305a22f616c2d954f
pablopr3 <pabloprdev@gmail.com>
2026-03-02 19:49:36 +0000

    variables

---
+++ clase_01_variables/ejercicio.py
@@ -0,0 +1,10 @@
+# --- PARTE 4: Ejercicio Final (Combinándolo todo) ---
+# 1. Pedimos el nombre al usuario
+usuario_nombre = input("Dime tu nombre: ")
+
+# 2. Pedimos la comida favorita
+comida_favorita = input("¿Cuál es tu comida favorita? ")
+
+# 3. Mostramos el resultado usando una f-string
+# La 'f' permite que las llaves {} saquen el valor de las cajas
+print(f"Hola {usuario_nombre}, qué hambre me da saber que te gusta comer {comida_favorita}")
